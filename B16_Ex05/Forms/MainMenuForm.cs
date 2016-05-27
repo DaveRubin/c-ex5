010526a8088778cc812d136e9bf4db00f0277111
David Rubin <dudrubin@gmail.com>
2016-05-27 23:43:52 +0300

    Wrapped all texts and values into consts

--- B16_Ex05/Forms/MainMenuForm.cs
+++ B16_Ex05/Forms/MainMenuForm.cs
@@ -6,20 +6,21 @@
 
     public class MainMenuForm : Form
     {
-        private TextBox m_Player1NameTextBox;
-        private TextBox m_Player2NameTextBox;
-        private CheckBox m_IsHumanPlayerCheckBox;
-        private NumericUpDown m_RowsSelector;
-        private NumericUpDown m_ColumnsSelector;
-
+        private const string k_WindoeTitle = "Game settings";
         private const int k_SelectorWidth = 50;
         private const int k_BoardSizeSectionTop = 100;
         private const int k_Padding = 20;
         private const int k_InnerExtraPadding = 10;
         private const int k_TextBoxesLeftValue = 100;
+        private const int k_StartButtonWidth = 100;
 
         private readonly int r_minSizeVal;
         private readonly int r_maxSizeVal;
+        private TextBox m_Player1NameTextBox;
+        private TextBox m_Player2NameTextBox;
+        private CheckBox m_IsHumanPlayerCheckBox;
+        private NumericUpDown m_RowsSelector;
+        private NumericUpDown m_ColumnsSelector;
 
         public delegate void GameSettingsEventHandler(MainMenuGameSettingsArgs args);
 
@@ -29,6 +30,9 @@
         {
             r_minSizeVal = i_MinSizeValue;
             r_maxSizeVal = i_MaxSizeValue;
+
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Text = k_WindoeTitle;
 
             CreatePlayersSection();
             CreateBoardSizeSection();
@@ -43,11 +47,11 @@
             m_Player2NameTextBox = new TextBox();
             m_IsHumanPlayerCheckBox = new CheckBox();
 
-            playersTitleLabel.Text = "Players:";
+            playersTitleLabel.Text = GameTexts.k_MainMenuFormPlayersTitleLabel;
             playersTitleLabel.Left = k_Padding;
             playersTitleLabel.Top = k_Padding;
 
-            Player1Label.Text = "Player1 :";
+            Player1Label.Text = GameTexts.k_MainMenuFormPlayer1Label;
             Player1Label.Left = k_Padding + k_InnerExtraPadding;
             Player1Label.Top = playersTitleLabel.Top + playersTitleLabel.Height;
 
@@ -56,12 +60,12 @@
 
             m_IsHumanPlayerCheckBox.Top = Player1Label.Top + Player1Label.Height;
             m_IsHumanPlayerCheckBox.Left = k_Padding + k_InnerExtraPadding;
-            m_IsHumanPlayerCheckBox.Text = "Player2 :";
+            m_IsHumanPlayerCheckBox.Text = GameTexts.k_MainMenuFormPlayer2Label;
 
             m_Player2NameTextBox.Left = k_TextBoxesLeftValue;
             m_Player2NameTextBox.Top = m_IsHumanPlayerCheckBox.Top;
             m_Player2NameTextBox.Enabled = false;
-            m_Player2NameTextBox.Text = "Computer";
+            m_Player2NameTextBox.Text = GameTexts.k_ComputerName;
 
             Controls.Add(m_Player2NameTextBox);
             Controls.Add(m_IsHumanPlayerCheckBox);
@@ -72,28 +76,48 @@
             m_IsHumanPlayerCheckBox.CheckedChanged += IsHumanPlayerCheckBox_CheckedChanged;
         }
 
+        /// <summary>
+        /// When checking\unchecking the player checkbox, enable\disable accordingly the text box 
+        /// and change to default computer name when nessecary
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void IsHumanPlayerCheckBox_CheckedChanged(object sender, System.EventArgs e)
         {
             m_Player2NameTextBox.Enabled = m_IsHumanPlayerCheckBox.Checked;
             if (!m_IsHumanPlayerCheckBox.Checked)
             {
-                m_Player2NameTextBox.Text = "Computer";
+                m_Player2NameTextBox.Text = GameTexts.k_ComputerName;
             }
         }
 
         private void CreateBoardSizeSection()
         {
             Label boardSizeSectionLabel = new Label();
-            m_RowsSelector = CreateSelector("Rows:", k_Padding + k_InnerExtraPadding, k_BoardSizeSectionTop + boardSizeSectionLabel.Height);
-            m_ColumnsSelector = CreateSelector("Columns:", k_Padding + k_InnerExtraPadding + 120, k_BoardSizeSectionTop + boardSizeSectionLabel.Height);
+            m_RowsSelector = CreateSelector(
+                GameTexts.k_MainMenuFormRowsLabel,
+                k_Padding + k_InnerExtraPadding,
+                k_BoardSizeSectionTop + boardSizeSectionLabel.Height);
 
-            boardSizeSectionLabel.Text = "Board size";
+            m_ColumnsSelector = CreateSelector(
+                GameTexts.k_MainMenuFormColumnsLabel,
+                k_Padding + k_InnerExtraPadding + 120,
+                k_BoardSizeSectionTop + boardSizeSectionLabel.Height);
+
+            boardSizeSectionLabel.Text = GameTexts.k_MainMenuFormBoardSizeTitleLabel;
             boardSizeSectionLabel.Top = k_BoardSizeSectionTop;
             boardSizeSectionLabel.Left = k_Padding;
 
             Controls.Add(boardSizeSectionLabel);
         }
 
+        /// <summary>
+        /// Helper function to create numeric up down component + matching label at a certain position
+        /// </summary>
+        /// <param name="i_Label"></param>
+        /// <param name="i_Left"></param>
+        /// <param name="i_Top"></param>
+        /// <returns></returns>
         private NumericUpDown CreateSelector(string i_Label ,int i_Left, int i_Top)
         {
             NumericUpDown selector = new NumericUpDown();
@@ -119,8 +143,8 @@
         private void CreateStartSection()
         {
             Button startButton = new Button();
-            startButton.Text = "Start!";
-            startButton.Width = 100;
+            startButton.Text = GameTexts.k_MainMenuStartButtonText;
+            startButton.Width = k_StartButtonWidth;
 
             startButton.Left = ClientSize.Width /2 - startButton.Width / 2;
             startButton.Top = ClientSize.Height - startButton.Height - k_Padding;
@@ -128,7 +152,13 @@
             Controls.Add(startButton);
         }
 
-
+        /// <summary>
+        /// When start clicked , gather information from components 
+        /// dispatch OnStartPressed event
+        /// and close window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void StartButton_Clicked(object sender, System.EventArgs e)
         {
             MainMenuGameSettingsArgs settings = new MainMenuGameSettingsArgs();
@@ -143,6 +173,7 @@
             {
                 OnStartPressed(settings);
             }
+
             Close();
         }
     }
--- B16_Ex05/GameTexts.cs
+++ B16_Ex05/GameTexts.cs
@@ -9,11 +9,21 @@ namespace B16_Ex05
     /// </summary>
     internal class GameTexts
     {
+        //labels
+        public const string k_MainMenuFormPlayer1Label = "Player1";
+        public const string k_MainMenuFormPlayer2Label = "Player2";
+        public const string k_MainMenuFormPlayersTitleLabel = "Players :";
+        public const string k_MainMenuFormRowsLabel = "Rows:";
+        public const string k_MainMenuFormColumnsLabel = "Columns:";
+        public const string k_MainMenuFormBoardSizeTitleLabel = "Board Size :";
+        public const string k_MainMenuStartButtonText = "Start!";
+
+
         public const string k_ColumnIsntFreeMessageTemplate = "Column {0} isn't free, please choose another";
         public const string k_playersTurnTextTemplate = "{0}'s turn";
         public const string k_Player1Name = "Player1";
         public const string k_Player2Name = "Player2";
-        public const string k_ComputerName = "COMPUTER";
+        public const string k_ComputerName = "Computer";
         public const string k_DimensionsInputTemplate = "Please enter number of {0} between {1} and {2}";
         public const string k_MenuHeaderTemplate =
 @"========================================================
